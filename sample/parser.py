28c62b291ecb912cba158ecf36b2c6d65152255f
Marcin Wątroba <markowanga@gmail.com>
2024-02-05 00:42:53 +0100

    Update example

--- sample/parser.py
+++ sample/parser.py
@@ -1,29 +1,34 @@
 from pathlib import Path
-from typing import AsyncGenerator, Any
+from typing import AsyncGenerator, Any, Optional, Dict
 
 from fastapi import FastAPI
-from sse_starlette import EventSourceResponse
+from pydantic import BaseModel
+from sse_starlette import EventSourceResponse, ServerSentEvent
 from starlette.responses import HTMLResponse
 
-from json_part import fix_incomplete_json
+from json_part import parse_incomplete_json
 from openai_sample import get_openai_stream_agenerator
 
 app = FastAPI()
 
 
-async def get_response_generator() -> AsyncGenerator[str, None]:
+class DescriptionEvent(BaseModel):
+    is_finish: bool
+    value: Optional[Any]
+
+
+async def get_response_generator() -> AsyncGenerator[ServerSentEvent, None]:
     accumulator = ""
     previous_json = None
     async for it in await get_openai_stream_agenerator():
         if it.choices[0].delta.tool_calls:
             accumulator += it.choices[0].delta.tool_calls[0].function.arguments
-            json = fix_incomplete_json(accumulator)
+            json = parse_incomplete_json(accumulator)
             if previous_json != json:
                 previous_json = json
-                yield json
+                yield ServerSentEvent(DescriptionEvent(is_finish=False, value=json).json())
         else:
-            break
-    print('finish get_response_generator')
+            yield ServerSentEvent(DescriptionEvent(is_finish=True, value=None).json())
 
 
 @app.get('/stream')
